63c6ac72ff7c9b936f0d4a6499695772530b820e
andre <andrei.tsizikov00@gmail.com>
2026-03-02 15:53:52 +0200

    bbbbbbb

---
+++ src/main/java/module6_1/main.java
@@ -0,0 +1,9 @@
+package module6_1;
+
+import module6_1.View.DictionaryView;
+
+public class main {
+    public static void main(String[] args) {
+        DictionaryView.launch(DictionaryView.class);
+    }
+}
